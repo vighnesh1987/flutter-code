9c9098605ee516a86581cff7b56591c4ab44cd22
vighnesh1987 <vighnesh1987@gmail.com>
2012-10-06 23:54:21 -0700

    Read in width and height

--- differentiate.cpp
+++ differentiate.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
+void parseArguments(int argc, char* argv[], int* wptr, int* hptr);
 
-int main() {
-
-  cout << "Hello World!";
-
+int main(int argc, char* argv[]) {
+  int  width, height;
+  parseArguments(argc, argv, &width, &height);
+  cout << width << height << " "<< height*width << endl;
+  return 0;
 }
+
+void parseArguments(int argc, char* argv[], int* wptr, int* hptr) {
+  if (argc != 3) {
+    cout << "Usage: Invoke as below\n" << argv[0] << " <width> <height>" << endl;
+    exit(1);
+  }
+  stringstream sw(argv[1]); sw >> *wptr;
+  stringstream sh(argv[2]); sh >> *hptr;
+}
+
+
