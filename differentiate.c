f0e3278440d2aa77f3e5600c4c94495e4443c52b
vighnesh1987 <vighnesh1987@gmail.com>
2012-10-07 01:06:30 -0700

    Created a struct for matrix

--- differentiate.c
+++ differentiate.c
@@ -3,48 +3,65 @@
 #include<time.h>
 #define RANGE 50
 
-void parseArguments(int argc, char* argv[], int* wptr, int* hptr);
-unsigned char** generateRandomMatrix(int width, int height);
-void printMatrix( unsigned char** matrix, int width, int height);
+typedef struct {
+  int width;
+  int height;
+  unsigned char** data;
+} Matrix;
+
+void parseArguments(int argc, char* argv[], Matrix* matrix);
+void initMatrix(Matrix* matrix);
+void fillRandomData(Matrix* matrix); 
+void printMatrix(Matrix* matrix); 
 
 int main(int argc, char* argv[]) {
 
   int width, height;
-  parseArguments(argc, argv, &width, &height);
-  unsigned char** matrix = generateRandomMatrix(width, height);
-  printMatrix(matrix, width, height);
+  Matrix matrix;
+  parseArguments(argc, argv, &matrix);
+  initMatrix(&matrix);
+  fillRandomData(&matrix);
+  printMatrix(&matrix);
 
+  /*unsigned char** dx = differentiateX(&matrix);*/
   return 0;
 }
 
-void parseArguments(int argc, char* argv[], int* wptr, int* hptr) {
+void parseArguments(int argc, char* argv[], Matrix* matrix) {
   if (argc != 3) {
     printf("Usage: Invoke as below\n  %s <width> <height>\n", argv[0]);
     exit(1);
   }
-  *wptr = atoi(argv[1]);
-  *hptr = atoi(argv[2]);
+  matrix->width = atoi(argv[1]);
+  matrix->height = atoi(argv[2]);
 }
 
-unsigned char** generateRandomMatrix(int width, int height) {
+void initMatrix(Matrix* matrix) {
+  matrix->data = malloc(sizeof(unsigned char*) * (matrix->width) );
+  for (int x = 0; x < matrix->width; x++) {
+    matrix->data[x] = malloc(sizeof(unsigned char) * (matrix->height) );
+  }
+}
+
+void fillRandomData(Matrix* matrix) {
   srand ( time(NULL) );
-  unsigned char** matrix = malloc(sizeof(unsigned char*)*width);
-  for (int x = 0; x < width; x++) {
-    matrix[x] = malloc(sizeof(unsigned char)*height);
-    for (int y = 0; y < height; y++) {
-      matrix[x][y] = rand() % RANGE + 1;
+  for (int x = 0; x < matrix->width; x++) {
+    for (int y = 0; y < matrix->height; y++) {
+      matrix->data[x][y] = rand() % RANGE + 1;
     }
   }
-  return matrix;
 }
 
-void printMatrix( unsigned char** matrix, int width, int height) {
-  for (int y = height - 1; y >= 0; y--) {
-    for (int x = 0; x < width; x++) {
-      printf("%5u ", matrix[x][y]);
+void printMatrix( Matrix* matrix) {
+  for (int y = matrix->height - 1; y >= 0; y--) {
+    for (int x = 0; x < matrix->width; x++) {
+      printf("%5u ", matrix->data[x][y]);
     }
     printf("\n");
   }
 }
 
+unsigned char** differentiateX(unsigned char** matrix, int width, int height) {
+
+}
 
